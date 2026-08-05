701b3373a995a117235b02dcd7caffa7a3899e15
ndeme yvan <ndemeyvan@gmail.com>
2026-08-05 14:45:35 +0100

    fix lint error

--- mobile_flutter/test/widget_test.dart
+++ mobile_flutter/test/widget_test.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:mobile_flutter/core/di/injection_container.dart';
 import 'package:mobile_flutter/main.dart';
 
 void main() {
   testWidgets('Payment screen renders correctly', (WidgetTester tester) async {
+    await init();
     await tester.pumpWidget(const PayCashApp());
     await tester.pumpAndSettle();
 
